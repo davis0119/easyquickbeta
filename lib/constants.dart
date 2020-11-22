e66f4c8675e9043dfdae101f53cf9f9309d0e1f4
deprevilor <deprevilor@gmail.com>
2020-11-21 18:33:22 -0600

    first commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF388E3C);
 const kPrimaryLightColor = Color(0xFF81C784);
+
+const kAnimationDuration = Duration(milliseconds: 200);
